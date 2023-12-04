f25a2a93cadfaab56f0d80254ffdef8c975a8a5f
uRexxy <rafinhamoraes1234@gmail.com>
2023-12-04 16:18:51 -0300

    Novos exs

---
+++ ExercíciosUDEMY/EX. adiarFuncao.py
@@ -0,0 +1,14 @@
+# Exercício - Adiando execução de funções
+from time import sleep
+def soma(x, y):
+    return x + y
+
+def multiplica(x, y):
+    return x * y
+
+def criar_funcao(funcao, *args):
+    return funcao(*args)
+
+
+soma_com_cinco = criar_funcao(soma, 5)
+multiplica_por_dez = criar_funcao(multiplica, 10)
--- ExercíciosUDEMY/EX. deepcopy.py
+++ ExercíciosUDEMY/EX. deepcopy.py
@@ -17,17 +17,26 @@ for dic in novos_produtos:
     novos_precos = dic.get('preco') + (dic.get('preco') * 10 / 100)
     novos_precos = f'{novos_precos:.2f}'
     dic.update({'preco': novos_precos})
-
+print('Produtos com 10% de aumento')
 for n in novos_produtos:
     print(n)
 
 # Ordene os produtos por nome decrescente (do maior para menor)
 
-
-
-
+nomes_decrescentes = sorted(novos_produtos, key=lambda dict: dict['nome'])
+print('Produtos em ordem decrescente')
+for n in nomes_decrescentes:
+    print(n)
 
 # Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 
+produtos_descrescentes = copy.deepcopy(nomes_decrescentes)
+
 # Ordene os produtos por preco crescente (do menor para maior)
+
+precos_crescentes = sorted(novos_produtos, key=lambda dict: dict['preco'])
+print('Precos em ordem crescente')
+for n in precos_crescentes:
+    print(n)
+
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
