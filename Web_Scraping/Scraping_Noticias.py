b03ffcb9a508de5bb21260f9cc3a7e0871dc70a4
uRexxy <rafinhamoraes1234@gmail.com>
2024-04-03 17:34:47 -0300

    Novos testes

--- Web_Scraping/Scraping_Noticias.py
+++ Web_Scraping/Scraping_Noticias.py
@@ -2,13 +2,11 @@ import requests
 from bs4 import BeautifulSoup as bs4
 
 
-header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0'}
-link = 'https://www.leroymerlin.com.br/pagina-de-ofertas-promocoes'
-response = requests.get(link, headers=header)
+link = 'https://g1.globo.com'
+response = requests.get(link).content
 print(response)
 
-site = bs4(response.text, 'html.parser')
+site = bs4(response, 'html.parser')
 
-nome_produto = site.find('span', class_='enof0xo0')
-print(nome_produto)
+noticia = site.find('span', class_='bstn-hl-title')
+print(noticia.string)
